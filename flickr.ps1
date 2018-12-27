ee8cc6789f6e74970c773673941bb52bc523ca26
Alvin Tran <alvin@atran.nl>
2018-12-28 00:16:54 +0100

    Update flickr.ps1

--- flickr.ps1
+++ flickr.ps1
@@ -1,12 +1,12 @@
 Import-Module BitsTransfer
 
 $FirstUrl = "https://s.yimg.com/kt/d/data_26100360_8d1fdb7b4d1b3f812f60832e903b62bba94d76dce68be80cc2ed5f10be0dbee7_1.zip"
-$BaseUrl = $FirstUrl.TrimEnd("1.zip")
+$FileRange = 1..51
 $BaseFolder = "C:\Users\alvin\Pictures"
 
 Write-Host "BEGIN TIME:" $(Get-Date)
 
-$FileRange = 1..51
+$BaseUrl = $FirstUrl.TrimEnd("1.zip")
 $FileRange | ForEach-Object {
 
     $url = ("$BaseUrl" + $_ + ".zip")
@@ -14,6 +14,7 @@ $FileRange | ForEach-Object {
     Write-Host $url
     Write-Host $output
 
+    ## uncomment to actualy download files
     #Start-BitsTransfer -Source $url -Destination $output    
 
 }
